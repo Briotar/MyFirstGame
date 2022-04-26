192c71e5a8a8859549a1f707e5cee22b1f8bb868
Briotar <vladimirklevtsovmain@gmail.com>
2022-04-26 18:49:49 +0300

    version 1.11
    Refactoring

--- Assets/Scripts/Coin/CointManager.cs
+++ Assets/Scripts/Coin/CointManager.cs
@@ -7,9 +7,5 @@ public class CointManager : ObjectManager
     protected override void Update()
     {
         base.Update();
-        //спавн монеток более рандомно
-        /*Vector3 min = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
-        Vector3 max = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, Camera.main.nearClipPlane));
-        */
     }
 }
--- Assets/Scripts/Rocket/RocketManager.cs
+++ Assets/Scripts/Rocket/RocketManager.cs
@@ -6,15 +6,6 @@ public class RocketManager : ObjectManager
 {
     [SerializeField] private ExplosionSpawner _explosionSpawner;
 
-    private int _currentRocketCount;
-
-    protected override void Start()
-    {
-        base.Start();
-
-        _currentRocketCount = 0;
-    }
-
     protected override void SetObject(GameObject rocketObject)
     {
         var rocket = rocketObject.GetComponent<Rocket>();
